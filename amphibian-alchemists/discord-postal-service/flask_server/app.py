c70d1dfde0cd90914dfc40dc23e933a6388aa323
chilaxan <35645806+chilaxan@users.noreply.github.com>
2020-01-23 23:12:53 -0500

    Update app.py

--- amphibian-alchemists/discord-postal-service/flask_server/app.py
+++ amphibian-alchemists/discord-postal-service/flask_server/app.py
@@ -1,14 +1,26 @@
 import os
 
+from threading import Thread
+from queue import Queue
+
 from dotenv import load_dotenv
 
-from . import app, db, discord_thread
+from . import app, db, discord_client
 
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 env_path = os.path.join(PROJECT_DIR, ".env")
 
+DISCORD_PERMISSION_INT = 268658688
+
+
+
+
 if __name__ == "__main__":
+    message_queue = Queue()
+    app.message_queue = message_queue
+    discord_client.message_queue = message_queue
     load_dotenv(dotenv_path=env_path)
     app.run()
     db.create_all()
+    discord_thread = Thread(target=discord_client.run, args=(os.getenv("DISCORD_BOT_TOKEN",))
     discord_thread.start()
